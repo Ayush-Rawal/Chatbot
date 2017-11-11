e4f22e642707a59a6c43c3b50bb51713ad62b6cd
Ayush Rawal <31813648+Oguz-Khagan@users.noreply.github.com>
2017-11-12 01:17:14 +0530

    same as prev

--- Todo List/todo.js
+++ Todo List/todo.js
@@ -2,5 +2,10 @@ angular.module("Todo_List", [])
     .controller("enter_input", enter_input)
 
 function enter_input() {
+    input = "";
+    this.tasks = [];
+    this.addtask = function(t) {
+        this.tasks.push(t);
+    }
 
 }
