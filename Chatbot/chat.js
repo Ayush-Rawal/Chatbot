25fece81c3b4eadd65c2ef7dd7a21977a28ec7c1
Ayush Rawal <31813648+Oguz-Khagan@users.noreply.github.com>
2017-11-12 12:59:12 +0530

    genderized

--- Chatbot/chat.js
+++ Chatbot/chat.js
@@ -2,7 +2,7 @@ angular.module("Chatbot", ["firebase"])
     .controller("chatctrl", chatctrl)
 
 
-function chatctrl($firebaseArray, $firebaseAuth) { //auth for login, array for.. well array
+function chatctrl($firebaseArray, $firebaseAuth, $http) { //auth for login, array for.. well array
     var chat = this;
     var auth = $firebaseAuth();
 
@@ -10,12 +10,21 @@ function chatctrl($firebaseArray, $firebaseAuth) { //auth for login, array for..
     chat.messages = $firebaseArray(chatref);
 
     chat.send = function(t, n) {
-        chat.messages.$add({ "text": t, "name": n })
+        chat.messages.$add({ "text": t, "name": chat.name, "photo": chat.photo })
     }
     chat.login = function() {
         // login with Facebook, we've used google
         auth.$signInWithPopup("google").then(function(firebaseUser) {
             console.log("Signed in as:", firebaseUser.user);
+            console.log(firebaseUser.user.displayName, firebaseUser.user.photoURL)
+            chat.name = firebaseUser.user.displayName;
+            chat.photo = firebaseUser.user.photoURL;
+
+            var url = "https://api.genderize.io?name=peter"
+            $http.get(url).then(function(data) {
+                console.log(data);
+            })
+
         }).catch(function(error) {
             console.log("Authentication failed:", error);
         });
