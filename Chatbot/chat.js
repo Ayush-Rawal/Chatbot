5503f160dc44a72ccb9c9f96c7013dcd44e281d4
Ayush Rawal <31813648+Oguz-Khagan@users.noreply.github.com>
2017-11-12 10:50:50 +0530

    Chatbot started, first chat sent to firebase database

---
+++ Chatbot/chat.js
@@ -0,0 +1,14 @@
+angular.module("Chatbot", ["firebase"])
+    .controller("chatctrl", chatctrl)
+
+
+function chatctrl($firebaseArray) {
+    var chat = this;
+
+    var chatref = firebase.database().ref("chats") //if chats is not present it will be created
+    chat.messages = $firebaseArray(chatref);
+
+    chat.send = function(t) {
+        chat.messages.$add({ "text": t })
+    }
+}
